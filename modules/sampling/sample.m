2fd57aebc685a9dc7330cd99d374ba14a13eb438
Ziad <ziadabillama2006@gmail.com>
2025-10-16 11:45:49 +0300

    Update sample.m

--- modules/sampling/sample.m
+++ modules/sampling/sample.m
@@ -1,14 +1,23 @@
 function [t_sample, x_sample] = sample(t, xt, fs)
-% sample - Samples a continuous-time signal at a specified rate
+% SAMPLE - Samples a given continuous-time signal at rate fs.
+%
 % Inputs:
-%   t  - original time vector
-%   xt - original signal values at t
-%   fs - sampling frequency (Hz)
+%   t  : Time vector (row vector)
+%   xt : Signal samples corresponding to t
+%   fs : Sampling frequency (Hz)
+%
 % Outputs:
-%   t_sample - sampled time points
-%   x_sample - sampled signal values
+%   t_sample : Sampled time indices
+%   x_sample : Sampled signal values
 
-    Ts = 1/fs;                     % Sampling period
-    t_sample = t(1) : Ts : t(end);   % New sampled time points
-    x_sample = interp1(t, xt, t_sample, 'linear');  % Interpolate to find sample values
+    Ts = 1 / fs;                   % Sampling period
+    t_min = min(t);
+    t_max = max(t);
+
+    % Generate sampling instants
+    t_sample = t_min : Ts : t_max;
+
+    % Interpolate x(t) at sampling points
+    x_sample = interp1(t, xt, t_sample, 'linear');  % Linear interpolation
+
 end
